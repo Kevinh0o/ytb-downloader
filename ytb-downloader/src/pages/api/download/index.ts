a9a69d2a74661141277c03b4fc0206c437bd56b4
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-10-25 17:36:23 -0300

    Feat: Api response time limit.

--- ytb-downloader/src/pages/api/download/index.ts
+++ ytb-downloader/src/pages/api/download/index.ts
@@ -6,14 +6,3 @@ export default function handler(req: NextApiRequest , res: NextApiResponse){
     const serverAdapter = new NextAdapter(res, req);
     serverAdapter.executeController(GetMediaController.get);
 }
-
-//Next has a response limito up to 4MB, it can be disabled by setting the config below.
-export const config = {
-    api: {
-        responseLimit: false,
-        bodyParser: {
-            sizeLimit: false,
-          },
-        maxDuration: 60 * 2, //60 seconds * 2 = 2 minutes
-    },
-}
